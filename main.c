5c69ce372f1f00d53dba82a1e859164127cc256a
poloelo <p.gioria2304@gmail.com>
2025-11-27 02:16:36 +0100

    //

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-    int every_letters = _printf("%d\n", 22);
+    int every_letters = _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
     _printf("%i", every_letters);
 }
